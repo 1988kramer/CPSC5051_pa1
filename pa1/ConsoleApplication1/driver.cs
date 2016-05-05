97e179f27fe00926940095da711f9c8b8d3aea31
Andrew Kramer <1988kramer@gmail.com>
2016-05-05 15:58:37 -0700

    finalized and added comments to encryptWord.cs

---
+++ pa1/ConsoleApplication1/driver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+  class driver
+  {
+    public static void Main()
+    {
+      Random r = new Random();
+      encryptWord encrypt = new encryptWord(r, 2);
+      string word, result;
+      while (encrypt.getIsOn())
+      {
+        Console.Write("Enter a word to encrypt: ");
+        word = Console.ReadLine();
+        try
+        {
+          result = encrypt.encrypt(word);
+        }
+        catch (Exception exception)
+        {
+          Console.WriteLine(exception.Message);
+          continue;
+        }
+        Console.WriteLine("Encrypted word:  " + result);
+        Console.Write("\nGuess shift value? (enter -1 for no guess)  ");
+        int shift;
+        if (!int.TryParse(Console.ReadLine(), out shift))
+        {
+          Console.WriteLine("Your guess was not a number.");
+          continue;
+        }
+        Console.WriteLine();
+        if (shift >= 0)
+        {
+          try
+          {
+            encrypt.guess(shift);
+          }
+          catch (Exception exception)
+          {
+            Console.WriteLine(exception.Message);
+            continue;
+          }
+        }
+      }
+      Console.WriteLine("Press enter to close:");
+      Console.ReadLine();
+    }
+  }
+}
